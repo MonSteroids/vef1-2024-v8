2615736ebb0a65b1092c359f5a8f1e4829ab0ac9
MonSteroids <112619594+MonSteroids@users.noreply.github.com>
2024-10-22 19:23:35 +0000

    viðbætur

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
+import { longest } from './lib/longest.js';
+import { shortest } from './lib/shortest.js';
+import { reverse } from './lib/reverse.js';
+import { palindrome } from './lib/palindrome.js';
+import { consonants } from './lib/consonants.js';
+import { vowels } from './lib/vowels.js';
+import { splitOnWhitespace } from './lib/helpers.js';
+
 /**
  * Sýnilausn á verkefni 8 í Vefforritun 1, 2024.
  * Byggir á sýnilausn á verkefni 7.
@@ -6,25 +14,43 @@
  * Notar `console.assert` til að athuga hvort föll virki rétt.
  */
 
-import { isString, splitOnWhitespace } from './lib/helpers.js';
+const textarea = document.getElementById('string');
+const outputElement = document.querySelector('.output');
+const longestEl = document.querySelector('.longest');
+const shortestEl = document.querySelector('.shortest');
+const vowelsEl = document.querySelector('.vowels');
+const consonantsEl = document.querySelector('.consonants');
+const palindromeEl = document.querySelector('.palindrome');
+const reversedEl = document.querySelector('.reversed');
+const inputEl = document.querySelector('.input');
+const greiningButton = document.getElementById('submit');
 
-const test = isString('hæ');
-console.log('test er strengur?', test);
-
-const stringWithWhitespace = `halló
-\theimur
-hæ`;
-const split = splitOnWhitespace(stringWithWhitespace);
-console.log(split);
-
-const textareaElement = document.querySelector('textarea');
-const formElement = document.querySelector('form');
-
-function submitHandler(event) {
-  event.preventDefault();
-  const { target } = event;
-  const textareaElement = target.querySelector('textarea');
-  console.log('textareaElemt inniheldur', textareaElement.value);
+/**
+ * 
+ * @param {Element} el 
+ */
+function removeHidden(el) {
+  el.classList.remove('hidden');
 }
 
-formElement.addEventListener('submit', submitHandler);
+if (outputElement) {
+  removeHidden(outputElement);
+}
+
+greiningButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const inputString = textarea.value;
+  const words = splitOnWhitespace(inputString);
+
+  // Fékk aðstoð frá chatGPt hérna.
+  // OpenAI. (2024). ChatGPT (22. okt. útgáfa v2) [Risamállíkan]. https://chat.openai.com/chat
+
+  inputEl.textContent = inputString;
+  longestEl.textContent = longest(inputString);
+  shortestEl.textContent = shortest(inputString);
+  vowelsEl.textContent = vowels(inputString);
+  consonantsEl.textContent = consonants(inputString);
+  palindromeEl.textContent = palindrome(inputString);
+  reversedEl.textContent = reverse(inputString);
+});
